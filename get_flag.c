be951534335da30e20e02c7f12b2c9f1b03197e1
PolarisABU <oluwadunsinolonitola@gmail.com>
2022-08-09 08:56:51 -0700

    Printf

--- get_flag.c
+++ get_flag.c
@@ -8,20 +8,20 @@
  */
 int get_flag(char s, flags_t *f)
 {
-	int i = 0;
-	switch (s)
-	{
-	case '+':
-	f->plus = 1;
-	i = 1;
-	break;
-	case ' ':
-	f->space = 1;
-	i = 1;
-	break;
-	case '#':
-	f->hash = 1;
-	i = 1;
-	break;
-	}
+int i = 0;
+switch (s)
+{
+case '+':
+f->plus = 1;
+i = 1;
+break;
+case ' ':
+f->space = 1;
+i = 1;
+break;
+case '#':
+f->hash = 1;
+i = 1;
+break;
+}
 }
